4103104fa0b34358a027e863a997f6d2fa1f31eb
rafattacks <r.edlmann@gmail.com>
2020-03-24 14:34:44 +0000

    bowling score

---
+++ VR Game Test/Assets/Scenes/BowlingPins.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingPins : MonoBehaviour
+{
+    public Transform pin;
+    public float threshold = .6f;
+    public int point = 1;
+    public BowlingScore score;
+
+
+    private void Awake()
+    {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<BowlingScore>();
+    }
+
+    void CheckifFail()
+    {
+        try
+        {
+            if (pin.up.y < threshold)
+            {
+                score.Add(point);
+                gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+        catch
+        {
+            Debug.Log("Looks like the pin went into the Dead Zone!");
+
+        }
+        
+            
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        CheckifFail();
+    }
+}
